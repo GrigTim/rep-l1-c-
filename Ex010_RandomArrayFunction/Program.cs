b5b0889da1fa89a742d6d7e77f0f51579f0cf4fe
Grig <timofeevgr19@mail.ru>
2023-07-31 17:30:43 +0300

    add random find array function

---
+++ Ex010_RandomArrayFunction/Program.cs
@@ -0,0 +1,46 @@
+﻿// переделка предыдущего с использованием рандома и функций. 
+// Этот метод заполняет массив на 10 цифирь псевдорандомным способом
+void FillArray (int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random().Next(1,10);
+        index++;
+    }
+}
+// а этот метод его типа показывает
+void PrintArray (int [] col)
+{
+    int count = col.Length; 
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine (col[position]);
+        position++;
+    }
+}
+int IndexOf (int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index ++;
+    }
+    return position;
+}
+int [] array = new int [10];
+FillArray (array);
+PrintArray (array);
+Console.WriteLine();
+
+int pos = IndexOf (array, 4);
+Console.WriteLine (pos);
