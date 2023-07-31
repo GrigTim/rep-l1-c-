e8887c10275de25e8edc1f4e2b2b350da39d347c
Grig <timofeevgr19@mail.ru>
2023-07-31 13:47:45 +0300

    find needed element in array

---
+++ example010_Methodarray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Старый добрый обычный поиск нужного элемента перебором в массиве
+int [] array = {1,2,17,18,17};
+int n = array.Length;
+
+int find = 17;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine (index);
+        //break прерывает цикл - то есть после первого найденного нужного элемента цикл остановится
+        break;
+    }
+    index ++;
+}
+
